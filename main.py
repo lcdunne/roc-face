801d76a09095648128ff847129657d49f0c24aaf
lcdunne <56237933+lcdunne@users.noreply.github.com>
2022-07-17 09:36:40 +0100

    Cleanup

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ import matplotlib.pyplot as plt
 import legacy_chi2 as legacy_stats
 
 # Utility funcs. ------------------------------------------------- #
+def array_like(x):
+    return isinstance(x, (list, tuple, np.ndarray,))
+
+def arrays_equal_length(a, b):
+    if len(a) != len(b):
+        return False
+    else:
+        return True
+
 def accumulate(arr):
+    if not array_like(arr):
+        raise ValueError("Expected array like")
     return np.cumsum(arr)
 
 def corrected_proportion(a, x, i=0):
@@ -25,12 +36,6 @@ def compute_proportions(arr, truncate=True):
 
     return np.array(freq)
 
-def arrays_equal_length(a, b):
-    if len(a) != len(b):
-        return False
-    else:
-        return True
-
 def plot_roc(signal, noise, ax=None, chance=True, **kwargs):
     if ax is None:
         fig, ax = plt.subplots()
@@ -45,24 +50,20 @@ def plot_roc(signal, noise, ax=None, chance=True, **kwargs):
 
 # Fitting functions
 def loglik(O, E, N):
-    # G-test: 
+    # G-test (https://en.wikipedia.org/wiki/G-test)
     with np.errstate(divide='ignore'):
         # ignore infinite value warning & return inf anyway.
         return 2 * O * np.log(O/E) + 2 * (N - O) * np.log((N - O)/(N - E))
 
+def chitest(O, E, N):
+    return (O - E)**2 / E + ((N-O) - (N-E))**2 / (N-E)
+
 def sum_sse(O, E):
     return sum( (O - E)**2 )
 
-def AIC(model=None, LL=None, k=None,):
-    if model is not None:
-        O = np.concatenate([model.p_signal, model.p_noise])
-        E = np.concatenate([model.expected_p_signal, model.expected_p_noise])
-        with np.errstate(divide='ignore'):
-            logdiff = np.log(np.abs(O - E))
-        logdiff[logdiff == -np.inf] = 0
-        LL = -(logdiff).sum()
-        k = model.k
-    return 2 * k - 2 * np.log(LL)
+def aic(L, k):
+    # k: number of estimated parameters in the model
+    return 2 * k - 2 * np.log(L)
 
 
 class BaseModel:
@@ -95,24 +96,17 @@ class BaseModel:
         self.p_signal = compute_proportions(self.signal)
         self.p_noise = compute_proportions(self.noise)
         
-        # Since multiple models will have criteria, best to init in parent class
-        if self.has_criteria:
+        # Since multiple models will have criteria, best to init them in this parent class
+        if self._has_criteria:
             self.n_criteria = len(self.p_signal)
             self._criteria = {
                 f"c{i}": {'initial': 0, 'bounds': (None, None)} for i in range(self.n_criteria)
             }
-            # self.parameters['criteria'] = np.zeros(len(self.signal))
-            # self.criterion_bounds = [(None, None) for _ in range(len(self.signal))]
-            # self.parameters = self.parameters | {f"c{i+1}": 0 for i in range(len(self.signal)-1)}
-            # self.parameter_boundaries += [(None, None) for _ in range(len(self.signal)-1)]
             self._parameters = self._named_parameters | self._criteria
         else:
-            # Typically will only be the case for high threshold model
+            # This will only be the case for high threshold model
             self.n_criteria = 0
             self._parameters = self._named_parameters.copy()
-        
-        # self.parameter_labels = list(self._parameters.keys())
-        # self.k = len(self.parameter_labels)
     
     @property
     def initial_parameters(self):
@@ -141,8 +135,7 @@ class BaseModel:
         return list(self.initial_parameters.values())
     
     def define_model_inputs(self, labels, values, n_criteria=0):
-        # Must map a flat list like [0.999, 0, 0, 0, 0, 0]
-        # to {'R': 0.999, 'criteria': [0,0,0,0,0]}
+        # Maps from flat list of labels and x0 values to dict accepted by `<model>.compute_expected`
         if n_criteria == 0:
             return dict(zip(labels, values))        
         # Number of named i.e. non-criteria parameters
@@ -153,16 +146,6 @@ class BaseModel:
         if withkeys is None:
             return {k: v for k, v in d.items()}
         return {k: v for k, v in d.items() if k in withkeys}
-    
-    # def define_model_inputs(self, x0, ignore_criteria=False):
-    #     # Makes dict to unpack as kwargs to the fit function.
-    #     model_input = {k: v for k, v in zip(self.parameter_labels, x0[:self.non_c_idx])}
-    #     if self.has_criteria and not ignore_criteria:
-    #         model_input['criteria'] = x0[self.non_c_idx:]
-    #     return model_input
-    
-    # def get_non_c(self):
-    #     return {k: v for k, v in self.fitted_parameters.items() if k in self.non_c_labels}
 
     def objective(self, x0, method='log-likelihood'):
         '''
@@ -185,7 +168,6 @@ class BaseModel:
         observed_noise = self.acc_noise[:-1]
         
         # Define the model inputs.
-        # Maps from flat list of labels and x0 values to dict accepted by `<model>.compute_expected`
         model_input = self.define_model_inputs(
             labels=self.parameter_labels,
             values=x0, # Not the same as self.x0: this one gets updated
@@ -202,10 +184,16 @@ class BaseModel:
 
         # Compute the fit statistic given observed and model-expected data
         if method == 'log-likelihood':
-            # Use the G-test
+            # Use the G^2-test
             ll_signal = loglik(O=observed_signal, E=expected_signal, N=self.n_signal)
             ll_noise = loglik(O=observed_noise, E=expected_noise, N=self.n_noise)
             return sum(ll_signal + ll_noise)
+        
+        elif method == 'chi':
+            # Use the chi^2 test
+            chi_signal = chitest(observed_signal, expected_signal, self.n_signal)
+            chi_noise = chitest(observed_noise, expected_noise, self.n_noise)
+            return sum(chi_signal + chi_noise)
 
         elif method == 'sse':
             # Fit using approach from Yonelinas' spreadsheet
@@ -255,24 +243,40 @@ class BaseModel:
         self.expected_p_noise, self.expected_p_signal = self.compute_expected(
             **self._fitted_parameters
         )
+        # TODO: Define nice results output
+        
+        # Errors
+        self._signal_squared_errors = (self.p_signal - self.expected_p_signal) ** 2
+        self._noise_squared_errors = (self.p_noise - self.expected_p_noise) ** 2
+        self.squared_errors = np.concatenate(
+            [self._signal_squared_errors, self._noise_squared_errors]
+        )
+        
+        if any(self.squared_errors==0):
+            self.squared_errors = self.squared_errors[self.squared_errors != 0]
+        self.aic = aic(L=sum(-np.log(np.sqrt(self.squared_errors))), k=self.n_param)
+        
         return self._fitted_parameters
     
     def euclidean_misfit(self, ox, oy, ex, ey):
+        # TODO
         pass
 
 
 class HighThreshold(BaseModel):
     __modelname__ = 'High Threshold'
-    has_criteria = False
+    _has_criteria = False
 
     def __init__(self, signal, noise):
         self._named_parameters = {'R': {'initial': 0.999, 'bounds': (0, 1)}}
+        # TODO: This model actually has 2 parameters, with `g` (guess). Currently self.n_param returns just 1. would be good to fix the compute_expected for this.
         super().__init__(signal, noise)
     
     def __repr__(self):
         return f"<{sdt.__class__.__name__}: {self.__modelname__}>"
     
     def compute_expected(self, R, full=False):
+        print(full)
         if full:
             model_noise = np.array([0, 1])
         else:
@@ -283,15 +287,15 @@ class HighThreshold(BaseModel):
 
 class SignalDetection(BaseModel):
     __modelname__ = 'Equal Variance Signal Detection'
-    has_criteria = True
+    _has_criteria = True
 
     def __init__(self, signal, noise, equal_variance=True):
-        self._named_parameters = {'d': {'initial': 0, 'bounds': (None, None)}}        
+        self._named_parameters = {'d': {'initial': 0, 'bounds': (None, None)}}
+        
         if not equal_variance:
             self.__modelname__ = self.__modelname__.replace('Equal', 'Unequal')
-            # Add additional param for the signal variance & its boundary
             self._named_parameters['scale'] = {'initial': 1, 'bounds': (1, None)}
-        # Create a meaningful label for reference
+
         self.label = ''.join([i[0] for i in self.__modelname__.split()])
         super().__init__(signal, noise)
     
@@ -301,8 +305,8 @@ class SignalDetection(BaseModel):
     # SDT function to get f_exp
     def compute_expected(self, d=None, scale=1, criteria=None):
         if criteria is None:
-            # Useful for plotting a smoothed curve
             criteria = np.arange(-5, 5, 0.01)
+
         model_signal = stats.norm.cdf(d / 2 - np.array(criteria), scale=scale)
         model_noise = stats.norm.cdf(-d / 2 - np.array(criteria), scale=1)
         return model_noise, model_signal
@@ -310,11 +314,10 @@ class SignalDetection(BaseModel):
 if __name__ == '__main__':
     signal = [505,248,226,172,144,93]
     noise = [115,185,304,523,551,397]
-    sdt = SignalDetection(signal, noise, equal_variance=False)
+    sdt = SignalDetection(signal, noise, equal_variance=True)
     sdt.fit()
     print(sdt.optimisation_output)
-    # TODO: There is a lot of junk in the code, needs refining.
-    
+
     ht = HighThreshold(signal, noise)
     ht.fit()
     print(ht.optimisation_output)
